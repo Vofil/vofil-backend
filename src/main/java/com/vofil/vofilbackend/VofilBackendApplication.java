ea469ec6eeb4d193e54f0b92ee9428430153ddc5
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-10-06 23:58:31 +0900

    initial commit

---
+++ src/main/java/com/vofil/vofilbackend/VofilBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vofil.vofilbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VofilBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VofilBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vofil/vofilbackend/VofilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vofil.vofilbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VofilBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
