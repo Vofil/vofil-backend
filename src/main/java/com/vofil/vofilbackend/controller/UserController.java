b60cd1e6573c581e6dc3dd311e7c27f020de2d5d
sherlock_notebook <syt06162@naver.com>
2022-10-31 20:36:19 +0900

    fix: user 로그인,회원가입 시 service, controller 로직 이동 service에서 ID비번일치 , ID존재여부 등 판단하도록.

--- src/main/java/com/vofil/vofilbackend/controller/UserController.java
+++ src/main/java/com/vofil/vofilbackend/controller/UserController.java
@@ -17,26 +17,12 @@ public class UserController {
 
     @PostMapping("")
     public ResponseEntity insertUser(@RequestBody User user) { //Json 형식
-        if (userService.findUserById(user.getId()).isPresent()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 존재하는 ID 입니다.");
-        }
-        else {
-            return ResponseEntity.ok().body(userService.signUp(user));
-        }
+        return userService.signUp(user);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity logInUser(@PathVariable String id, @RequestBody User user) {
-        Optional<User> foundUser = userService.findUserById(id);
-        if (foundUser.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("존재하지 않는 ID 입니다.");
-        }
-        else if (!foundUser.get().getPassword().equals(user.getPassword())){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ID와 비밀번호가 일치하지 않습니다.");
-        }
-        else {
-            return ResponseEntity.ok().body(userService.logIn(id, user.getPassword())); // 그냥 foundUser를 줘도 되지만 추후에 보안 등 작업은 Service 단에서 할수도
-        }
+        return userService.logIn(id, user.getPassword());
     }
 
     @GetMapping("")
--- src/main/java/com/vofil/vofilbackend/service/UserService.java
+++ src/main/java/com/vofil/vofilbackend/service/UserService.java
@@ -2,6 +2,8 @@ package com.vofil.vofilbackend.service;
 
 import com.vofil.vofilbackend.domain.User;
 import com.vofil.vofilbackend.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -16,18 +18,34 @@ public class UserService {
     }
 
     // 회원가입
-    public String signUp(User user){
-        userRepository.insertUser(user);
-        return user.getName();
+    public ResponseEntity signUp(User user){
+
+        if (findUserById(user.getId()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 존재하는 ID 입니다.");
+        }
+        else {
+            userRepository.insertUser(user);
+            return ResponseEntity.ok().body(user.getName());
+        }
     }
     public Optional<User> findUserById(String id) {
         return userRepository.findById(id);
     }
 
     // 로그인
-    public Optional<User> logIn(String id, String password) {
-        // 로그인 추가 로직 - JWT, 보안 등 필요시
-        return userRepository.findById(id);
+    public ResponseEntity logIn(String id, String password) {
+
+        Optional<User> foundUser = findUserById(id);
+        if (foundUser.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("존재하지 않는 ID 입니다.");
+        }
+        else if (!foundUser.get().getPassword().equals(password)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ID와 비밀번호가 일치하지 않습니다.");
+        }
+        else {
+            // 로그인 추가 로직 - JWT, 보안 등 필요시
+            return ResponseEntity.ok().body(userRepository.findById(id));
+        }
     }
 
 
