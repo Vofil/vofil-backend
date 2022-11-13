f6f6d486a466332a56ad65ca3ea063d7c6f58863
sherlock_notebook <syt06162@naver.com>
2022-11-13 15:59:19 +0900

    feat: vote updateUserTitle 함수 진행중

--- src/main/java/com/vofil/vofilbackend/repository/VoteRepository.java
+++ src/main/java/com/vofil/vofilbackend/repository/VoteRepository.java
@@ -1,8 +1,11 @@
 package com.vofil.vofilbackend.repository;
 
 import com.vofil.vofilbackend.domain.Vote;
+import com.vofil.vofilbackend.domain.Voter;
+import com.vofil.vofilbackend.vote.VoteFeeling;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -56,4 +59,107 @@ public class VoteRepository {
     /*public static Vote findById(int id) {
         return store.get(id);
     }*/
+
+
+
+    // vote 하나가 끝나면, 그에 해당하는 voter들의 칭호를 업데이트한다.
+    public void updateUserTitle(List<Voter> voters) {
+        // 그에 해당하는 voter들의 모든 칭호를 업데이트 (해당되는 애들만)
+        for (int i = 0 ; i<voters.size() ; i++) {
+            Voter nowVoter = voters.get(i);
+            List<Voter> nowVoterResult = em.createQuery("select u from Voter u where u.Voter_id = :id",Voter.class).setParameter("id",nowVoter.getId()).getResultList();
+
+            // 매 X번 마다, 칭호를 업데이트한다.
+            int XXX = 1;
+            if (nowVoterResult.size() % XXX == 0) {
+                // 대중, 독창 판단 변수
+                int isMatched = 0; // 대중적인지 (결과랑 match 됐는지)
+                int isNotMatched = 0; // 독창적인지 (결과랑 match 안됐을때)
+
+                // 상황, 분위기, 종류 판별 변수
+                int situationCnt = 0;
+                int moodCnt = 0;
+                int kindCnt = 0;
+
+                // 각각의 vote에 대해, 1.대중/독창 계산 , 2.상황/분위기/종류 계산
+                for (int j = 0 ; j<nowVoterResult.size() ; j++) {
+                    Vote nowVoterVote = findById(nowVoterResult.get(j).getVote_id()).get(); // vote 추출
+
+                    // 아직 안 끝난 투표는 패스
+                    if (nowVoterVote.getEnding_point() != 0) continue;
+
+                    // 1. 대중/독창 계산
+                    int[] resultArray = new int[4];
+                    resultArray[0] = nowVoterVote.getResult1();
+                    resultArray[1] = nowVoterVote.getResult2();
+                    resultArray[2] = nowVoterVote.getResult3();
+                    resultArray[3] = nowVoterVote.getResult4();
+                    int maxResult = -1;
+                    for (int k = 0; k<4; k++) {
+                        if (maxResult < resultArray[k]) maxResult = resultArray[k];
+                    }
+                    ArrayList<Integer> maxList = new ArrayList<>();
+                    for (int k = 0; k<4; k++) {
+                        if (maxResult == resultArray[k]) maxList.add(k);
+                    }
+
+                    // 내가 투표한 것의 숫자
+                    int myResult = -1;
+                    if (nowVoterResult.get(j).getResult1() == 1) myResult = 1;
+                    else if (nowVoterResult.get(j).getResult2() == 1) myResult = 2;
+                    else if (nowVoterResult.get(j).getResult3() == 1) myResult = 3;
+                    else myResult = 4;
+
+                    boolean notInFlag = true;
+                    for (int k = 0; k<maxList.size(); k++) {
+                        if (maxList.get(i) == myResult){
+                            notInFlag = false;
+                            isMatched += 1;
+                            break;
+                        }
+                    }
+                    if (notInFlag) isNotMatched += 1;
+
+                    // 2. 상황/분위기/종류 계산
+                    VoteFeeling titles = VoteFeeling.valueOf(nowVoterVote.getFeeling());
+                    if (titles.number()<10){
+                        situationCnt += 1;
+                    }
+                    else if(titles.number()<100){
+                        moodCnt += 1;
+                    }
+                    else{
+                        kindCnt += 1;
+                    }
+                }
+
+                // 변수들 값들 비교해서 칭호 최종 판별
+                String title = "기본 칭호";
+                int[] results = {isMatched, isNotMatched, situationCnt, moodCnt, kindCnt};
+                int maxVal = -1;
+                for (int l = 0; l< results.length; l++) {
+                    if (maxVal < results[l]) maxVal = results[l];
+                }
+
+                if (situationCnt == maxVal) title= "상황을 잘 보는 눈";
+                else if (moodCnt == maxVal) title = "분위기를 잘 보는 눈";
+                else if (kindCnt == maxVal) title = "종류를 잘 보는 눈";
+                else if (isMatched == maxVal) title = "대중적인 눈";
+                else if (isNotMatched == maxVal) title = "독창적인 눈";
+
+                // 그 유저의 칭호 UPDATE 쿼리
+                // todo update 쿼리
+            }
+        }
+    }
+
+    public int max(int a, int b) {
+        if (a>b) return a;
+        else return b;
+    }
+    public int abs(int a) {
+        if (a>=0) return a;
+        else return -a;
+    }
 }
+
