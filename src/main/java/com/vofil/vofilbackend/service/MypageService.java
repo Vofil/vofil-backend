9cd44a47d9f3c6b77d1641d221611808840c1a26
sherlock_notebook <syt06162@naver.com>
2022-11-17 14:36:36 +0900

    feat: mypage 유저정보, 유저칭호 api 개발 완료

--- src/main/java/com/vofil/vofilbackend/service/MypageService.java
+++ src/main/java/com/vofil/vofilbackend/service/MypageService.java
@@ -1,6 +1,8 @@
 package com.vofil.vofilbackend.service;
 
+import com.vofil.vofilbackend.domain.MypageUserInformation;
 import com.vofil.vofilbackend.domain.SimpleVoteInformation;
+import com.vofil.vofilbackend.domain.User;
 import com.vofil.vofilbackend.domain.Vote;
 import com.vofil.vofilbackend.repository.MainpageRepository;
 import com.vofil.vofilbackend.repository.MypageRepository;
@@ -17,6 +19,8 @@ public class MypageService {
     MypageRepository mypageRepository;
     @Autowired
     private MainpageRepository mainpageRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public MypageService(MypageRepository mypageRepository) {
         this.mypageRepository=mypageRepository;
@@ -34,6 +38,28 @@ public class MypageService {
     public List<SimpleVoteInformation> getUserVoters(String user_id){
         List<Vote> votes = mypageRepository.getUserVoters(user_id);
         return mainpageRepository.getSimpleVoteInformationList(votes);
+    }
+
+    // MypageUserInformation
+    public MypageUserInformation getUserInformation(String user_id) {
+        User user = userRepository.findById(user_id).get();
+
+        MypageUserInformation ui = new MypageUserInformation();
+        ui.setId(user_id);
+        ui.setName(user.getName());
+        ui.setTitle(user.getTitle());
+        ui.setGender(user.getGender());
+
+        String birth = String.valueOf(user.getBirth_year()) +  "-" + String.valueOf(String.format("%02d", user.getBirth_month())) + "-"
+                + String.valueOf(String.format("%02d", user.getBirth_day()));
+        ui.setBirth(birth);
+        return ui;
+    }
+
+    // 칭호만 리턴
+    public String getUserTitle(String user_id) {
+        User user = userRepository.findById(user_id).get();
+        return user.getTitle();
     }
 
 }
