67edc3dea059c60f6210926aa67e8d607ac6c470
sherlock_notebook <syt06162@naver.com>
2022-12-08 02:30:05 +0900

    fix: 끌올, 투표생성 포인트 지급 값 수정. 투표생성:10, 참여:1, 끌올:-50

--- src/main/java/com/vofil/vofilbackend/controller/VoteController.java
+++ src/main/java/com/vofil/vofilbackend/controller/VoteController.java
@@ -82,7 +82,7 @@ public class VoteController {
 
     @Autowired
     UserService userService;
-    private final int RERAISE_COST = 5;
+    private final int RERAISE_COST = 50;
     @PutMapping(value = "/reraise", params = "userId")
     public int reraise(@RequestParam String userId) {
         if (userService.minusPointsIfHave(RERAISE_COST, userId)) {
@@ -90,7 +90,7 @@ public class VoteController {
             return voteService.reraise(randomVoteId); // 있으면 그 번호, 없으면 -1
         }
         else {
-            return -RERAISE_COST; // 포인트가 부족하면 -5
+            return -RERAISE_COST; // 포인트가 부족하면 -50
         }
 
     }
